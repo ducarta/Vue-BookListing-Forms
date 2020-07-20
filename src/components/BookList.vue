2c449f1e00efa976ed522fd9649bc0adcb5e04d8
ducarta <66382075+ducarta@users.noreply.github.com>
2020-07-20 13:06:40 +0100

    fixed a typo

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,7 +5,7 @@
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
-    <h2>Filtred Books By Ownership</h2>
+    <h2>Filtered Books By Ownership</h2>
     <select v-model="holding"><option v-for="filter in filters" :key="filter.id">{{ filter }}</option></select>
     <ul>
       <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
