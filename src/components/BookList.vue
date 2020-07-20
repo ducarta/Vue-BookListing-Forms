8d11d6de5a2ffc9c2880aaf82f740894caceb665
ducarta <66382075+ducarta@users.noreply.github.com>
2020-07-20 13:05:03 +0100

    removed comment

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -42,7 +42,6 @@ export default {
   computed: {
     filteredBooks() {
      return _.filter(this.books, ["ownership", this.holding]);
-      //return _.filter(this.books, ["finishedReading", true]);
     }
   },
   methods: {
